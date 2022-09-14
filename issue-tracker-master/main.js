bb14f3bb9c419a17ab8478901ea00f19f5a80e0e
Ashim Rudra Paul <ashimrudrapaul23@gmail.com>
2022-09-14 10:09:21 +0600

    error fixed

---
+++ issue-tracker-master/main.js
@@ -0,0 +1,60 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+  const getInputValue = id => document.getElementById(id).value;
+  const description = getInputValue('issueDescription');
+  const severity = getInputValue('issueSeverity');
+  const assignedTo = getInputValue('issueAssignedTo');
+  const id = Math.floor(Math.random() * 100000000) + '';
+  const status = 'Open';
+
+  const issue = { id, description, severity, assignedTo, status };
+  let issues = [];
+  if (localStorage.getItem('issues')) {
+    issues = JSON.parse(localStorage.getItem('issues'));
+  }
+  issues.push(issue);
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  document.getElementById('issueInputForm').reset();
+  fetchIssues();
+  e.preventDefault();
+}
+
+const closeIssue = (id) => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const currentIssue = issues.filter(issue => issue.id === id);
+  currentIssue.status = 'Closed';
+  localStorage.clear('issues', JSON.stringify(issues));
+  fetchIssues();
+  location.reload(true)
+}
+
+const deleteIssue = (id) => {
+  console.log(id)
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  console.log(issues)
+  const remainingIssues = issues.filter(issues => issues.id !== id);
+  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  location.reload(true)
+}
+
+const fetchIssues = () => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issuesList = document.getElementById('issuesList');
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    const { id, description, severity, assignedTo, status } = issues[i];
+
+    issuesList.innerHTML += `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
+                              </div>`;
+  }
+}
